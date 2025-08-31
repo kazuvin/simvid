da947cd4da80e026b7c906cce54c5912c88b008f
kazuvin <166889742+kazuvin@users.noreply.github.com>
2025-08-31 17:55:44 +0900

    delete: unnecessary files

--- packages/web/app/_index.tsx
+++
@@ -1,9 +0,0 @@
-import { SITE_TITLE } from '~/config';
-
-export function meta() {
-  return [{ title: SITE_TITLE }, { name: 'description', content: `Welcome to ${SITE_TITLE}` }];
-}
-
-export default function Home() {
-  return <div className="flex min-h-screen items-center justify-center">{SITE_TITLE}</div>;
-}
